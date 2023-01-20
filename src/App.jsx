63cb133f9067ad6f5fd0296cbd1461186472757d
Fernando Gimenez <fernandoagimenez@gmail.com>
2023-01-21 00:24:45 +0100

    home v0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from '@mui/material/styles'
-import { theme } from './styles/styles'
+import { theme } from './styles/theme'
 import { Home } from './components/Home'
 import './App.css'
 import { DrawerAppBar } from './components/NavBar/DraweAppBar'
+import { Container } from '@mui/material'
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <DrawerAppBar />
-      <Home />
+        <Container maxWidth='xl'>
+            <DrawerAppBar />
+            <Home />
+        </Container>
     </ThemeProvider>
   )
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,30 @@
-import { Box } from "@mui/material"
+import { 
+    Offset,
+    HomeContainer,
+    HomeContent,
+    HomeText,
+    HomeTextAlicante,
+    HomeButton
+ } from "../../styles/home";
 
+ import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
+const Home = () => {
 
- const Home = () => {
   return (
-      <Box >
-          <h1>Home</h1>
-      </Box>
+      <HomeContainer>
+        <Offset />
+        <HomeContent>
+            <HomeText variant="subtitle">
+                A delicious way to discover <br></br>
+                <HomeTextAlicante variant='span'>
+                Alicante
+                </HomeTextAlicante>
+            </HomeText>
+            <HomeButton variant="contained" endIcon={<ArrowDropDownCircleIcon />}>
+                See itinerary
+            </HomeButton>
+        </HomeContent>
+      </HomeContainer>
   )
 }
 
